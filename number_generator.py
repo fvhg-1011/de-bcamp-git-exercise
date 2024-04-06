c71ed766ea91de2f46977df2011cd874e308de7b
Favian Hugo <hugo.sulistiawan@gmail.com>
2024-04-07 07:13:55 +0800

    fix(number-generator): add squared function

--- number_generator.py
+++ number_generator.py
@@ -1,4 +1,11 @@
 import random
 
-random_number = random.randint(1,6)
-print(random_number)
+random_number = random.randint(1, 6)
+
+
+def squared_number(x):
+    return x**2
+
+
+squared_random_number = squared_number(random_number)
+print(squared_random_number)
